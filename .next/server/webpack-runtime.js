5b67ad97749d6d14d48d1d8f99722a313fbd7dc4
EnyoyX-HQ <devops@tryenvoyx.com>
2025-06-30 03:45:34 +0100

    Update font size to 14px

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d65d97d68f226801")
+/******/ 		__webpack_require__.h = () => ("62a65cff7adfa94a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
