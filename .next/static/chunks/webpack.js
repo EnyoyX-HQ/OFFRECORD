785a831da882d4936cae01a169abe210a35cd259
EnyoyX-HQ <devops@tryenvoyx.com>
2025-06-30 04:14:27 +0100

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7972986717159ba6"; }
+/******/ 		__webpack_require__.h = function() { return "4be1393712297051"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
