c06af7ea29ec32a7c756c2c327fcf4fa81c635bd
EnyoyX-HQ <devops@tryenvoyx.com>
2025-06-30 03:18:31 +0100

    Fix import paths to resolve 404 errors

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0ba16596dcc22dc0")
+/******/ 		__webpack_require__.h = () => ("bbe2bfc22e861433")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "77241de273243578"; }
+/******/ 		__webpack_require__.h = function() { return "7919002584621a68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
